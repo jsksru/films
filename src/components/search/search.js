8d389047ac22dec1620a82d7bcdd9b89149ab3dc
jsksru <mail@jsks.ru>
2020-07-08 13:17:26 +0500

    Made simple component "Search bar"

---
+++ src/components/search/search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <div className="search">
+      <input type="text" placeholder="Search by film title..."/>
+      <button type="button">Search</button>
+    </div>
+  );
+}
+
+export default Search;
