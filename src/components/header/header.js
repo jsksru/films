83cff770e1e4ba88779b3b7fc3eec0b8b42a3306
jsksru <mail@jsks.ru>
2020-07-08 17:12:25 +0500

    Added some styles for Heading component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './header.css';
 
 const Header = () => {
   return (
     <header className="heading">
-      <h1>Films</h1>
+      <h1 className="heading__title">Films</h1>
     </header>
   );
 }
