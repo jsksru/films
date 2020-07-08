63e781ff6f08818878501932775a93cbe2f73098
jsksru <mail@jsks.ru>
2020-07-08 15:41:45 +0500

    Hardcoded simple films id's to list

--- src/components/films-list/films-list.js
+++ src/components/films-list/films-list.js
@@ -6,16 +6,40 @@ const FilmsList = () => {
   return (
     <ul className="films">
       <li>
-        <FilmCard/>
+        <FilmCard id="tt0105690"/>
       </li>
       <li>
-        <FilmCard/>
+        <FilmCard id="tt0114781"/>
       </li>
       <li>
-        <FilmCard/>
+        <FilmCard id="tt0099785"/>
       </li>
       <li>
-        <FilmCard/>
+        <FilmCard id="tt0104431"/>
+      </li>
+      <li>
+        <FilmCard id="tt0088247"/>
+      </li>
+      <li>
+        <FilmCard id="tt0103064"/>
+      </li>
+      <li>
+        <FilmCard id="tt0088763"/>
+      </li>
+      <li>
+        <FilmCard id="tt0096874"/>
+      </li>
+      <li>
+        <FilmCard id="tt0099088"/>
+      </li>
+      <li>
+        <FilmCard id="tt0109830"/>
+      </li>
+      <li>
+        <FilmCard id="tt0111161"/>
+      </li>
+      <li>
+        <FilmCard id="tt0110357"/>
       </li>
     </ul>
   );
