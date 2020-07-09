148a2f21639a6865e47c805a4c430708e51432aa
jsksru <mail@jsks.ru>
2020-07-09 12:54:22 +0500

    Added 2s debouncing search and log result to console

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './search.css';
+import api from '../../api/films-api';
+
+const SEARCH_INTERVAL = 2000;
 
 const Search = () => {
+  let isSearchStarted = false;
+
+  const inputHandler = (e) => {
+    const inputEl = e.target;
+
+    if (!isSearchStarted && inputEl.value !== '' && inputEl.value.length > 1) {
+      isSearchStarted = true;
+      setTimeout(async() => {
+        const response = await api.searchByTitle(inputEl.value);
+        console.log(response);
+        isSearchStarted = false;
+      }, SEARCH_INTERVAL);
+    }
+    
+  }
+
   return (
     <div className="search">
-      <input className="search__input" type="text" placeholder="Search by film title..."/>
+      <input className="search__input" type="text" placeholder="Search by film title..." onChange={inputHandler}/>
     </div>
   );
 }
