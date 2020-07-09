1840391d7ca332e2fc79dafb1c2ec0b3641788e3
jsksru <mail@jsks.ru>
2020-07-09 12:52:45 +0500

    Added find by text test

--- src/api/films-api.test.js
+++ src/api/films-api.test.js
@@ -1,6 +1,11 @@
 import api from './films-api';
 
-it('API is get data about films by IMDB id', async () => {
-  const request = await api.getById('tt3896198');
-  expect(request.data.Title).toBe('Guardians of the Galaxy Vol. 2');
+it('API is get data about movie by IMDB id', async () => {
+  const response = await api.getById('tt3896198');
+  expect(response.Title).toBe('Guardians of the Galaxy Vol. 2');
+});
+
+it('API is find movie by title', async () => {
+  const response = await api.searchByTitle('star');
+  expect(response.Title).toBe('Star Wars: Episode IV - A New Hope');
 });
