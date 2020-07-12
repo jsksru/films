ed14e463c3763cb6f7320918115f70efad0070bd
jsksru <mail@jsks.ru>
2020-07-12 14:34:14 +0500

    Little change syntax in test

--- src/api/films-api.test.js
+++ src/api/films-api.test.js
@@ -1,11 +1,13 @@
 import api from './films-api';
 
-it('API is get data about movie by IMDB id', async () => {
-  const response = await api.getById('tt3896198');
-  expect(response.Title).toBe('Guardians of the Galaxy Vol. 2');
-});
-
-it('API is find movie by title', async () => {
-  const response = await api.searchByTitle('star');
-  expect(response.Title).toBe('Star Wars: Episode IV - A New Hope');
+describe('test api', () => {
+  test('API is get data about movie by IMDB id', async () => {
+    const response = await api.getById('tt3896198');
+    expect(response.Title).toBe('Guardians of the Galaxy Vol. 2');
+  });
+  
+  test('API is find movie by title', async () => {
+    const response = await api.searchByTitle('star');
+    expect(response.Title).toBe('Star Wars: Episode IV - A New Hope');
+  });
 });
