b5c4ccf6638ec9f8d4cda68354e221ad5eabdb4a
jsksru <mail@jsks.ru>
2020-07-12 16:49:47 +0500

    Changed API (Fetch instead axios)

--- src/api/films-api.js
+++ src/api/films-api.js
@@ -1,29 +1,29 @@
-import axios from 'axios';
 const BASE_URL = `http://www.omdbapi.com/?apikey=760858f1`;
 
 class Api {
-  #instance = axios.create({
-    baseURL: BASE_URL,
-    timeout: 50000
-  });
 
   async getById(imdbID) {
-    const req = await this.#instance.get(null, {
-      params: {
-        i: imdbID
-      }
-    });
-    return req.data;
+    try {
+      const reqest = await fetch(`${BASE_URL}&i=${imdbID}`);
+      const data = await reqest.json();
+      return data;
+    }
+    catch {
+      return false;
+    }
   }
 
   async searchByTitle(title) {
-    const req = await this.#instance.get(null, {
-      params: {
-        t: title
-      }
-    });
-    return req.data;
+    try {
+      const reqest = await fetch(`${BASE_URL}&t=${title}`);
+      const data = await reqest.json();
+      return data;
+    }
+    catch {
+      return false;
+    }
   }
+
 }
 
 export default new Api;
