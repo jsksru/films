3fbb590579ace0d8dcd25f8d16619c239c14c734
jsksru <mail@jsks.ru>
2020-07-12 17:58:21 +0500

    Styled by Material-UI

--- src/components/film-card/film-card.js
+++ src/components/film-card/film-card.js
@@ -1,12 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import './film-card.css';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 import api from '../../api/films-api';
+
+
 
 const FilmCard = (props) => {
   const filmId = props.id || 'tt3896198';
   const [ isLoaded, setLoaded ] = useState(false);
   const [ filmData, setFilmData ] = useState({});
+
+  const useStyles = makeStyles({
+    root: {
+      maxWidth: '100%',
+    },
+    media: {
+      height: 300,
+    },
+  });
+
+  const classes = useStyles();
 
   useEffect(() => {
     api.getById(filmId)
@@ -39,15 +60,34 @@ const FilmCard = (props) => {
 
   const film = () => {
     return (
-      <div className="film">
-        <div className="film__rating">{filmData.rating}</div>
-        <img className="film__image" src={filmData.image} alt={filmData.title}/>
-        <div className="film__info">
-          <div>{filmData.country}</div>
-          <div>{filmData.year}</div>
-        </div>
-        <h2 className="film__title">{filmData.title}</h2>
-      </div>
+      <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={filmData.image}
+          title={filmData.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {filmData.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {filmData.country}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {filmData.year}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Add to Favorites
+        </Button>
+        <Button size="small" color="primary">
+          Learn more...
+        </Button>
+      </CardActions>
+    </Card>
     );
   };
 
--- src/components/films-list/films-list.js
+++ src/components/films-list/films-list.js
@@ -1,49 +1,64 @@
 import React from 'react';
-import './films-list.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 import FilmCard from '../film-card/film-card';
 
-const FilmsList = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function CenteredGrid() {
+  const classes = useStyles();
+
   return (
-    <ul className="films">
-      <li>
-        <FilmCard id="tt0105690"/>
-      </li>
-      <li>
-        <FilmCard id="tt0114781"/>
-      </li>
-      <li>
-        <FilmCard id="tt0099785"/>
-      </li>
-      <li>
-        <FilmCard id="tt0104431"/>
-      </li>
-      <li>
-        <FilmCard id="tt0088247"/>
-      </li>
-      <li>
-        <FilmCard id="tt0103064"/>
-      </li>
-      <li>
-        <FilmCard id="tt0088763"/>
-      </li>
-      <li>
-        <FilmCard id="tt0096874"/>
-      </li>
-      <li>
-        <FilmCard id="tt0099088"/>
-      </li>
-      <li>
-        <FilmCard id="tt0109830"/>
-      </li>
-      <li>
-        <FilmCard id="tt0111161"/>
-      </li>
-      <li>
-        <FilmCard id="tt0110357"/>
-      </li>
-    </ul>
+    <Container fixed>
+      <Grid container spacing={3}>
+        <Grid item xs={1} lg={3}>
+          <FilmCard id="tt0105690"/>
+        </Grid>
+        <Grid item xs={1} lg={3}>
+          <FilmCard id="tt0099785"/>
+          </Grid>
+        <Grid item xs={1} lg={3}>
+          <FilmCard id="tt0104431"/>
+          </Grid>
+        <Grid item xs={1} lg={3}>
+          <FilmCard id="tt0114781"/>
+          </Grid>
+        <Grid item xs={1} lg={3}>
+          <FilmCard id="tt0088247"/>
+          </Grid>
+        <Grid item xs={1} lg={3}>
+          <FilmCard id="tt0103064"/>
+          </Grid>
+        <Grid item xs={1} lg={3}>
+          <FilmCard id="tt0088763"/>
+          </Grid>
+        <Grid item xs={1} lg={4}>
+          <FilmCard id="tt0096874"/>
+          </Grid>
+        <Grid item xs={1} lg={3}>
+          <FilmCard id="tt0099088"/>
+          </Grid>
+        <Grid item xs={1} lg={3}>
+          <FilmCard id="tt0109830"/>
+          </Grid>
+        <Grid item xs={1} lg={3}>
+          <FilmCard id="tt0111161"/>
+          </Grid>
+        <Grid item xs={1} lg={3}>
+          <FilmCard id="tt0110357"/>
+          </Grid>
+      </Grid>
+    </Container>
   );
 }
-
-export default FilmsList;
